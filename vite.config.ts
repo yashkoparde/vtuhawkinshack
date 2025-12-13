181814858eb8599cea45e0a9d4cb9e141c7739cf
yashkoparde <yashkoparde2022@gmail.com>
2025-12-13 18:08:33 +0530

    fix vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/vtuhawkinscode/', // required for GitHub Pages
+    base: '/vtuhawkinshack/',
 
     plugins: [react()],
 
